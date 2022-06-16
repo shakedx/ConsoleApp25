b60ed75a4031d9eeb23dd0766c8b6b8c961e204f
shakedx <kolesnikov_danil123@mail.ru>
2022-06-16 23:30:22 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) Console.WriteLine("Год високосный");
+            else Console.WriteLine("Год не високосный");
+        }
+    }
+}
